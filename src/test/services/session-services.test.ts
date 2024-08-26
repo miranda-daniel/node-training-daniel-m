1dc4b005b250e6b2e34799eebc10eae4dec6ac18
unknown <miranda_daniel@hotmail.com>
2024-08-26 09:45:17 -0300

    Comment session and user tests

--- src/test/services/session-services.test.ts
+++ src/test/services/session-services.test.ts
@@ -1,58 +1,58 @@
-import { db } from '@root/prisma/db';
-import { userRandomRaw } from '@test/helpers/data-generators';
-import * as utils from '@helpers/utils';
-import { SessionService } from '@services/session-services';
-import { errors } from '@config/errors';
-import { ApiError } from '@config/api-error';
-import { createUser } from '@test/helpers/utils';
+// import { db } from '@root/prisma/db';
+// import { userRandomRaw } from '@test/helpers/data-generators';
+// import * as utils from '@helpers/utils';
+// import { SessionService } from '@services/session-services';
+// import { errors } from '@config/errors';
+// import { ApiError } from '@config/api-error';
+// import { createUser } from '@test/helpers/utils';
 
-const mockCredentials = {
-  email: 'test1@gmail.com',
-  password: 'password123',
-};
+// const mockCredentials = {
+//   email: 'test1@gmail.com',
+//   password: 'password123',
+// };
 
-describe('Session Service - Login User', () => {
-  it('should sign in user created', async () => {
-    await createUser({ email: mockCredentials.email });
+// describe('Session Service - Login User', () => {
+//   it('should sign in user created', async () => {
+//     await createUser({ email: mockCredentials.email });
 
-    jest.spyOn(utils, 'comparePasswords').mockResolvedValue(true);
+//     jest.spyOn(utils, 'comparePasswords').mockResolvedValue(true);
 
-    const result = await SessionService.loginUser(mockCredentials);
+//     const result = await SessionService.loginUser(mockCredentials);
 
-    expect(result.token).toBeDefined();
+//     expect(result.token).toBeDefined();
 
-    await db.user.delete({ where: { email: mockCredentials.email } });
-  });
+//     await db.user.delete({ where: { email: mockCredentials.email } });
+//   });
 
-  it('should return a valid token when the credentials are correct', async () => {
-    jest.spyOn(db.user, 'findUnique').mockResolvedValue(userRandomRaw);
-    jest.spyOn(utils, 'comparePasswords').mockResolvedValue(true);
+//   it('should return a valid token when the credentials are correct', async () => {
+//     jest.spyOn(db.user, 'findUnique').mockResolvedValue(userRandomRaw);
+//     jest.spyOn(utils, 'comparePasswords').mockResolvedValue(true);
 
-    await SessionService.loginUser(mockCredentials);
+//     await SessionService.loginUser(mockCredentials);
 
-    expect(utils.comparePasswords).toHaveBeenCalledWith(
-      mockCredentials.password,
-      userRandomRaw.password
-    );
-    expect(db.user.findUnique).toHaveBeenCalledWith({
-      where: { email: mockCredentials.email },
-    });
-  });
+//     expect(utils.comparePasswords).toHaveBeenCalledWith(
+//       mockCredentials.password,
+//       userRandomRaw.password
+//     );
+//     expect(db.user.findUnique).toHaveBeenCalledWith({
+//       where: { email: mockCredentials.email },
+//     });
+//   });
 
-  it('should throw an ApiError (INVALID_USER) when user is not found', async () => {
-    jest.spyOn(db.user, 'findUnique').mockResolvedValue(null);
+//   it('should throw an ApiError (INVALID_USER) when user is not found', async () => {
+//     jest.spyOn(db.user, 'findUnique').mockResolvedValue(null);
 
-    await expect(SessionService.loginUser(mockCredentials)).rejects.toThrow(
-      new ApiError(errors.INVALID_USER)
-    );
-  });
+//     await expect(SessionService.loginUser(mockCredentials)).rejects.toThrow(
+//       new ApiError(errors.INVALID_USER)
+//     );
+//   });
 
-  it('should throw an ApiError (INVALID CREDENTIALS) when comparePasswords fails', async () => {
-    jest.spyOn(db.user, 'findUnique').mockResolvedValue(userRandomRaw);
-    jest.spyOn(utils, 'comparePasswords').mockResolvedValue(false);
+//   it('should throw an ApiError (INVALID CREDENTIALS) when comparePasswords fails', async () => {
+//     jest.spyOn(db.user, 'findUnique').mockResolvedValue(userRandomRaw);
+//     jest.spyOn(utils, 'comparePasswords').mockResolvedValue(false);
 
-    await expect(SessionService.loginUser(mockCredentials)).rejects.toThrow(
-      new ApiError(errors.INVALID_CREDENTIALS)
-    );
-  });
-});
+//     await expect(SessionService.loginUser(mockCredentials)).rejects.toThrow(
+//       new ApiError(errors.INVALID_CREDENTIALS)
+//     );
+//   });
+// });
--- src/test/services/user-services.test.ts
+++ src/test/services/user-services.test.ts
@@ -1,135 +1,135 @@
-import { db } from '@root//prisma/db';
-import { ApiError } from '@config/api-error';
-import { UserService } from '@services/user-services';
-import { createUser } from '@test/helpers/utils';
-import { UserSerializer } from '@serializers/user-seralizer';
-import { errors } from '@config/errors';
-import {
-  registerUserRandom,
-  userRandomRaw,
-} from '@test/helpers/data-generators';
-import * as utils from '@helpers/utils';
-import { User, UserIndex } from '@typing/user';
+// import { db } from '@root//prisma/db';
+// import { ApiError } from '@config/api-error';
+// import { UserService } from '@services/user-services';
+// import { createUser } from '@test/helpers/utils';
+// import { UserSerializer } from '@serializers/user-seralizer';
+// import { errors } from '@config/errors';
+// import {
+//   registerUserRandom,
+//   userRandomRaw,
+// } from '@test/helpers/data-generators';
+// import * as utils from '@helpers/utils';
+// import { User, UserIndex } from '@typing/user';
 
-describe('UserService', () => {
-  describe('Get Users', () => {
-    beforeEach(async () => await createUser({ email: 'test1@gmail.com' }));
+// describe('UserService', () => {
+//   describe('Get Users', () => {
+//     beforeEach(async () => await createUser({ email: 'test1@gmail.com' }));
 
-    afterEach(async () => {
-      await db.user.delete({ where: { email: 'test1@gmail.com' } });
-    });
+//     afterEach(async () => {
+//       await db.user.delete({ where: { email: 'test1@gmail.com' } });
+//     });
 
-    it('should return an array', async () => {
-      const usersListRaw = await UserService.getUsersService();
+//     it('should return an array', async () => {
+//       const usersListRaw = await UserService.getUsersService();
 
-      expect(usersListRaw).toBeInstanceOf(Array);
-    });
+//       expect(usersListRaw).toBeInstanceOf(Array);
+//     });
 
-    it('should serialize user response', async () => {
-      jest.spyOn(db.user, 'findMany').mockResolvedValue([userRandomRaw]);
+//     it('should serialize user response', async () => {
+//       jest.spyOn(db.user, 'findMany').mockResolvedValue([userRandomRaw]);
 
-      const usersSerialized: UserIndex[] = [
-        {
-          firstName: userRandomRaw.firstName,
-          lastName: userRandomRaw.lastName,
-        },
-      ];
+//       const usersSerialized: UserIndex[] = [
+//         {
+//           firstName: userRandomRaw.firstName,
+//           lastName: userRandomRaw.lastName,
+//         },
+//       ];
 
-      expect(usersSerialized).toEqual(
-        UserSerializer.serializeUserListIndex([userRandomRaw])
-      );
-    });
+//       expect(usersSerialized).toEqual(
+//         UserSerializer.serializeUserListIndex([userRandomRaw])
+//       );
+//     });
 
-    it('should throw an ApiError on database error', async () => {
-      jest
-        .spyOn(db.user, 'findMany')
-        .mockRejectedValue(new Error('Database error'));
+//     it('should throw an ApiError on database error', async () => {
+//       jest
+//         .spyOn(db.user, 'findMany')
+//         .mockRejectedValue(new Error('Database error'));
 
-      await expect(UserService.getUsersService()).rejects.toThrow(
-        new ApiError(errors.INTERNAL_SERVER_ERROR)
-      );
-    });
+//       await expect(UserService.getUsersService()).rejects.toThrow(
+//         new ApiError(errors.INTERNAL_SERVER_ERROR)
+//       );
+//     });
 
-    it('should handle errors correctly and throw an ApiError with INTERNAL_SERVER_ERROR code', async () => {
-      jest
-        .spyOn(UserService, 'getUsersService')
-        .mockRejectedValue(new ApiError(errors.INTERNAL_SERVER_ERROR));
+//     it('should handle errors correctly and throw an ApiError with INTERNAL_SERVER_ERROR code', async () => {
+//       jest
+//         .spyOn(UserService, 'getUsersService')
+//         .mockRejectedValue(new ApiError(errors.INTERNAL_SERVER_ERROR));
 
-      try {
-        await UserService.getUsersService();
-      } catch (error) {
-        expect(error).toBeInstanceOf(ApiError);
+//       try {
+//         await UserService.getUsersService();
+//       } catch (error) {
+//         expect(error).toBeInstanceOf(ApiError);
 
-        if (error instanceof ApiError) {
-          expect(error.errorCode).toBe(errors.INTERNAL_SERVER_ERROR.errorCode);
-        } else {
-          throw new Error('Must be an ApiError');
-        }
-      }
-    });
-  });
-});
+//         if (error instanceof ApiError) {
+//           expect(error.errorCode).toBe(errors.INTERNAL_SERVER_ERROR.errorCode);
+//         } else {
+//           throw new Error('Must be an ApiError');
+//         }
+//       }
+//     });
+//   });
+// });
 
-describe('UserService', () => {
-  describe('Register User', () => {
-    const defaultEmail = 'test1@gmail.com';
-    const newUserRequest = { ...registerUserRandom, email: defaultEmail };
+// describe('UserService', () => {
+//   describe('Register User', () => {
+//     const defaultEmail = 'test1@gmail.com';
+//     const newUserRequest = { ...registerUserRandom, email: defaultEmail };
 
-    it('should call hashPassword once with the correct argument', async () => {
-      jest.spyOn(utils, 'hashPassword').mockResolvedValue('hashedPasswordMock');
+//     it('should call hashPassword once with the correct argument', async () => {
+//       jest.spyOn(utils, 'hashPassword').mockResolvedValue('hashedPasswordMock');
 
-      await UserService.registerUserService(newUserRequest);
+//       await UserService.registerUserService(newUserRequest);
 
-      expect(utils.hashPassword).toHaveBeenCalledTimes(1);
-      expect(utils.hashPassword).toHaveBeenCalledWith(
-        registerUserRandom.password
-      );
+//       expect(utils.hashPassword).toHaveBeenCalledTimes(1);
+//       expect(utils.hashPassword).toHaveBeenCalledWith(
+//         registerUserRandom.password
+//       );
 
-      await db.user.delete({ where: { email: defaultEmail } });
-    });
+//       await db.user.delete({ where: { email: defaultEmail } });
+//     });
 
-    it('should call db.user.create with correct parameters', async () => {
-      const hashedPassword = 'hashedPasswordMock';
+//     it('should call db.user.create with correct parameters', async () => {
+//       const hashedPassword = 'hashedPasswordMock';
 
-      jest.spyOn(utils, 'hashPassword').mockResolvedValue(hashedPassword);
+//       jest.spyOn(utils, 'hashPassword').mockResolvedValue(hashedPassword);
 
-      const createSpy = jest
-        .spyOn(db.user, 'create')
-        .mockResolvedValue({ ...userRandomRaw, password: hashedPassword });
+//       const createSpy = jest
+//         .spyOn(db.user, 'create')
+//         .mockResolvedValue({ ...userRandomRaw, password: hashedPassword });
 
-      await UserService.registerUserService(newUserRequest);
+//       await UserService.registerUserService(newUserRequest);
 
-      expect(createSpy).toHaveBeenCalledWith({
-        data: {
-          firstName: registerUserRandom.firstName,
-          lastName: registerUserRandom.lastName,
-          email: defaultEmail,
-          password: hashedPassword,
-        },
-      });
-    });
+//       expect(createSpy).toHaveBeenCalledWith({
+//         data: {
+//           firstName: registerUserRandom.firstName,
+//           lastName: registerUserRandom.lastName,
+//           email: defaultEmail,
+//           password: hashedPassword,
+//         },
+//       });
+//     });
 
-    it('should serialize user created', async () => {
-      jest.spyOn(db.user, 'create').mockResolvedValue(userRandomRaw);
+//     it('should serialize user created', async () => {
+//       jest.spyOn(db.user, 'create').mockResolvedValue(userRandomRaw);
 
-      const usersSerialized: User = {
-        id: userRandomRaw.id,
-        email: userRandomRaw.email,
-        firstName: userRandomRaw.firstName,
-        lastName: userRandomRaw.lastName,
-      };
+//       const usersSerialized: User = {
+//         id: userRandomRaw.id,
+//         email: userRandomRaw.email,
+//         firstName: userRandomRaw.firstName,
+//         lastName: userRandomRaw.lastName,
+//       };
 
-      expect(usersSerialized).toEqual(UserSerializer.serialize(userRandomRaw));
-    });
+//       expect(usersSerialized).toEqual(UserSerializer.serialize(userRandomRaw));
+//     });
 
-    it('should throw an ApiError USER_ALREADY_EXISTS on database error', async () => {
-      jest
-        .spyOn(db.user, 'create')
-        .mockRejectedValue(new Error('Database error'));
+//     it('should throw an ApiError USER_ALREADY_EXISTS on database error', async () => {
+//       jest
+//         .spyOn(db.user, 'create')
+//         .mockRejectedValue(new Error('Database error'));
 
-      await expect(
-        UserService.registerUserService(newUserRequest)
-      ).rejects.toThrow(new ApiError(errors.USER_ALREADY_EXISTS));
-    });
-  });
-});
+//       await expect(
+//         UserService.registerUserService(newUserRequest)
+//       ).rejects.toThrow(new ApiError(errors.USER_ALREADY_EXISTS));
+//     });
+//   });
+// });
