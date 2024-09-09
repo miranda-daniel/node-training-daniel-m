20e50a9720ff8326dfd1a55635afeff5b453aab9
unknown <miranda_daniel@hotmail.com>
2024-09-09 15:02:33 -0300

    Unify jobs.

--- src/test/jest.teardown.ts
+++ src/test/jest.teardown.ts
@@ -2,10 +2,6 @@ import { PrismaClient } from '@prisma/client';
 const db = new PrismaClient();
 
 module.exports = async () => {
-  if (process.env.TEST_ENV !== 'integration') {
-    return;
-  }
-
   // Define the tables in sequential order to be deleted after ALL tests.
   const tables = ['Product', 'User'];
 
